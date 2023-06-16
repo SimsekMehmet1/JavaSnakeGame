62be56ed03e7f34eacff2446bd6cdaf1377a031a
SimsekMehmet1 <simsek.mehmet1@outlook.com>
2023-06-16 15:23:31 +0100

    Created new method to populate apple on the screen Added variables Developed the startGame Method Created grids on both x and y axis to make it easier for us to visualize how big objects will be

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,14 +14,41 @@ import javax.swing.JPanel;
 // Implements ActionListener which listens for actions on the panel like button presses
 public class GamePanel extends JPanel implements ActionListener {
 
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	static final int UNIT_SIZE = 25; // This is how big the objects in the game will be
+	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE; // How many objects will fit on the screen
+	static final int DELAY = 75;
+
+	// Arrays hold all the cordinates for the body parts of the snake
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 6;
+	int appleX;
+	int appleY;
+	char direction = 'R'; // The snake will begin by going right
+	boolean running = false;
+	Timer timer;
+	Random random;
+
 // Constructor for the GamePanel class
 	GamePanel() {
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
 
 	}
 
 //Method to start the game. This need to be filled with game starting logic
 
 	public void startGame() {
+		newApple();
+		running = true;
+		timer = new Timer(DELAY, this); // Pass in delay value to dictate how fast the game is running
+										// We pass in this because we are using action listener interface
 
 	}
 
@@ -30,11 +59,29 @@ public class GamePanel extends JPanel implements ActionListener {
 
 	public void paintComponent(Graphics g) {
 
+		// grids on both x and y axis to make it easier for us to visualize how big
+		// objects will be
+
+		for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+			g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+		}
+
+		g.setColor(Color.red);
+		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
 	}
 
 	// Method for drawing game objects
 	// This should be called from gameCompinent method
 	public void draw(Graphics g) {
+
+	}
+
+	// This method creates a new apple on the screen every time its called
+	public void newApple() {
+		appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+		appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
 
 	}
 
